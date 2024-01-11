2c2822ed95123d49ebbd36e78235d331bbc8e6eb
sanatankc <sanatan.iit@gmail.com>
2024-01-11 14:28:27 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import netlify from '@astrojs/netlify/functions';
+
 
 import vercel from "@astrojs/vercel/serverless";
 
@@ -7,6 +9,9 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  // adapter: vercel(),
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   server: (command) => ({ port: command === 'dev' ? 4231 : 4000, host: true })
 });
